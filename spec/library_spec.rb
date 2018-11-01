655d1510b7b4dd8a7692577bd0f4a2e6aebaaa5a
Max Stevenson <maxstevenson@msn.com>
2018-11-01 10:14:05 +0000

    Rspec test written for second method of Library class
    Unit and function tests both successful

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,12 +1,27 @@
 require 'library'
 	
 describe Library do 
-
+	# Method name tested
 	describe '#list_books' do
+		# Explanation of expected method behavior
 		it 'can return a list of all stored books' do
+			# Arrange
 			library = Library.new
-
+			# Act and Assert
 			expect(library.list_books).to eq(library.books)
+		end
+	end
+	# Method name tested
+	describe '#add_book' do
+		# Explanation of expected method behavior
+		it 'adds a new book object to books' do
+			# Arrange
+			book = {title: 'I am a book', author: 'test', subject: 'test'}
+			library = Library.new
+			# Act
+			library.add_book(book)
+			# Assert
+			expect(library.list_books).to include(book)
 		end
 	end
 end
